e920f27e590de635baaed5ea8e359eeadac240ce
jjdiazr <jjdiazr@eafit.edu.co>
2026-02-18 18:54:32 -0500

    nueva pagina para cursos

--- roadmap/urls.py
+++ roadmap/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.specialization_list, name='specialization_list'),
+    path('specialization/<int:pk>/', views.specialization_detail, name='specialization_detail'),
+
 ]
