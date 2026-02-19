4333254af7d3bb4b842d682cf5a0deac0ca23841
jjdiazr <jjdiazr@eafit.edu.co>
2026-02-18 20:54:50 -0500

    barra de busqueda

--- roadmap/views.py
+++ roadmap/views.py
@@ -2,18 +2,23 @@ from django.shortcuts import render, get_object_or_404
 from .models import Specialization
 
 def specialization_list(request):
-    specializations = Specialization.objects.all()
+    query = request.GET.get('q', '')
+    specializations = Specialization.objects.filter(name__icontains=query)
     return render(request, 'roadmap/home.html', {
         'specializations': specializations,
-        
+        'query': query,
     })
+
 
 def specialization_detail(request, pk):
     specialization = get_object_or_404(Specialization, pk=pk)
     courses = specialization.courses.all()
+    courses_query = request.GET.get('q', '')
+    courses = courses.filter(name__icontains=courses_query)
     return render(request, 'roadmap/courses.html', {
         'specialization': specialization,
         'courses': courses,
+        'query': courses_query,
     })
 
 
