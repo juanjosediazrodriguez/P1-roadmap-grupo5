8d8a2680e5df2eabb07fcfe947d544175e09f3d1
Jjes07 <jjescobars@eafit.edu.co>
2026-02-21 01:59:58 -0500

    Busqueda en tiempo real agregada

--- roadmap/urls.py
+++ roadmap/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.specialization_list, name='specialization_list'),
     path('specialization/<int:pk>/', views.specialization_detail, name='specialization_detail'),
-
+    path('search/specializations/', views.specialization_search, name='specialization_search'),
+    path('specialization/<int:pk>/search/', views.course_search, name='course_search'),
+    
 ]
