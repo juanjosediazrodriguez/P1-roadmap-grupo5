c5ec6caaa16c363373f6e2c53cc33431c217c043
Jjes07 <jjescobars@eafit.edu.co>
2026-04-16 12:53:27 -0500

    Merged login and register from main

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('preferences/', views.preferences_view, name='preferences'),
     path('preferences/save/', views.save_preferences, name='save_preferences'),
+    path('preferences/delete/', views.delete_preferences, name='delete_preferences'),
 ]
