4d5554821f52c078597d8a889bd9007477ade1f4
LeidyRoldan15 <ldroldan0516@gmail.com>
2026-04-22 16:39:23 -0500

    Implementa perfil para agregar semestre actual del estudiante

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('preferences/', views.preferences_view, name='preferences'),
     path('preferences/save/', views.save_preferences, name='save_preferences'),
     path('preferences/delete/', views.delete_preferences, name='delete_preferences'),
+    path('profile/', views.profile_view, name='profile'),
 ]
